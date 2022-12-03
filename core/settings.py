06333954af47c45c1d03037e06968c474e5361fa
Martin Bernát <ber39288@spseol.cz>
2022-12-03 23:12:52 +0100

    Přidání whitenoise, Debug=False, Funkční static soubory 3

--- core/settings.py
+++ core/settings.py
@@ -116,3 +116,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS=['web-production-c8c7.up.railway.app']
