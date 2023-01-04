f25d3ff54df783db72e9d70ceeb05ba24f6a6ba4
Martin Bernát <ber39288@spseol.cz>
2023-01-04 21:04:31 +0100

    Pokus 2 o opravu CSRF

--- core/settings.py
+++ core/settings.py
@@ -15,8 +15,8 @@ SECRET_KEY = 'django-insecure-e6puspy$0v5q*!3da3f3#t(!jll*0b%j2#2jq@!2r0a4+scfnp
 DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'https://web-production-c8c7.up.railway.app']
-CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS.copy()
+# CSRF_COOKIE_SECURE = True
+# CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS.copy()
 
 
 # Application definition
